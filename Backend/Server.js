806d1184c2f058f56ebd5cd4fe96dacaabe4b7aa
Rituraj Rai <162144998+riturajrai@users.noreply.github.com>
2025-06-25 18:16:07 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -12,7 +12,7 @@ const faceUsersRoutes = require('./attendance.js/face-users')
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://admin.vocalheart.com', 'https://visits.vocalheart.com'  ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
